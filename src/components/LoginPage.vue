7b23b3f59309794c7355629ee8fcc81e585d2ad2
larsje99 <91471321+larsje99@users.noreply.github.com>
2023-10-24 12:12:58 +0200

    updated_login

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,78 +1,68 @@
 <template>
-    <div>
-      <header class="header">
-        <div class="container">
-          
-        <img src="../assets/image/StoryStreak_Logo.png" id="logo" alt="Logo" />
-          
-          <h1>StoryStreak</h1>
-          <nav class="nav">
-            <ul>
-              <li><router-link to="/Register">No account? -> Register</router-link></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-  
-      <div class="box">
-        <h1 id="title">Log in</h1>
-  
-        <br />
-        <label for="Username" id="user">Username</label>
-        <br />
-        <br />
-        <input
-          v-model="username"
-          type="username"
-          required
-          id="Username"
-          name="Username"
-          placeholder="Username"
-        />
-        <br />
-        <br />
-        <label for="Password" id="pass">Password</label>
-        <br />
-        <br />
-        <input
-          v-model="password"
-          type="password"
-          required
-          id="Password"
-          name="Password"
-          placeholder="Password"
-        />
-        <br />
-        <br />
-        <br />
-        <button @click="login" id="Log-in_button">Log In</button>
-        <br />
-        <br />
-        <br />
-      </div>
-      <br />
-      <br />
-      <br />
-      <footer class="footer">
-        <div class="copyright">&copy; by StoryStreak</div>
-      </footer>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'LoginPage',
-    data() {
-      return {
-        username: "",
-        password: "",
-      };
+  <div>
+    <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="../assets/image/StoryStreak_Logo_Trans.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form">
+					<span class="login100-form-title">
+						Member Login
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="###">
+							<router-link to="/Register">Create your Account</router-link>
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+  </div>
+</template>
+
+<script>
+import "../assets/css/login.css"
+import "../assets/css/util.css"
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Add your login logic here
+      // Access username and password using this.username and this.password
     },
-    methods: {
-      login() {
-        // Add your login logic here
-        // Access username and password using this.username and this.password
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -1,86 +1,81 @@
 <template>
-<body>
-    <header class="header">
-        <div class="container">
-            
-        <img src="../assets/image/StoryStreak_Logo.png" id="logo" alt="Logo">
-            
-            <h1>StoryStreak</h1>
-            <nav class="nav">
-                <ul>
-                    <li><router-link to="/Login">Already got an account? -> Login</router-link></li>
-                </ul>
-            </nav>
-        </div>
-    </header>        
-    
-    <div class="box">
-        <h1 id="title">
-            Register
-        </h1>
+    <div>
+        <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="../assets/image/StoryStreak_Logo_Trans.png" alt="IMG">
+				</div>
 
-        <form action="php" method="post">
-            <label for="Email" id="mail">
-            E-mail
-            </label>
-            <br>
-            <br>
-            <input type="email" required id="Email" name="Email" placeholder="E-mail">
-            <br>
-            <br>
-            <label for="username" id="user" required>
-                Username
-            </label>
-            <br>
-            <br>
-            <input type="text" id="username" name="username" placeholder="Your username" required>
-            <br>
-            <br>
-            <label for="Password" id="pass">
-                Password
-            </label>
-            <br>
-            <br>
-            <input type="password" id="Password" name="Password" placeholder="Password" required>
-            <br>
-            <br>
-            <router-link to="./loginPage.html">
-                <input id="registerButton" type="submit" value="Register">
-            </router-link>
-        </form>
-        <br>
+				<form class="login100-form validate-form">
+					<span class="login100-form-title">
+						Member Register
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid Username is required">
+						<input class="input100" type="text" name="email" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" placeholder="Email">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Create Account
+						</button>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2">
+                            Already have an account?
+							<router-link to="/Login">Go to Login</router-link>
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
     </div>
-    <br>
-    <br>
-    <br>
-    <footer class="footer">
-        <div class="copyright">
-            &copy; by StoryStreak
-        </div>
-    </footer>
-
-</body>
-
 </template>
   
-  <script>
-  import "../assets/css/front.css"
-  export default {
-    name: 'RegisterPage',
-    data() {
-      return {
-          comments: [],
-          newcomments: ""
-      };
-    },
-    
-    methods: {
-      addComment() {
-          if (this.newComment.trim() !== '') {
-              this.comments.push(this.newComment);
-              this.newComment = '';
-          }
-      }
+<script>
+import "../assets/css/front.css"
+import "../assets/css/login.css"
+import "../assets/css/util.css"
+export default {
+name: 'RegisterPage',
+data() {
+    return {
+        comments: [],
+        newcomments: ""
+    };
+},
+
+methods: {
+    addComment() {
+        if (this.newComment.trim() !== '') {
+            this.comments.push(this.newComment);
+            this.newComment = '';
+        }
     }
-  }
-  </script>
+}
+}
+</script>
